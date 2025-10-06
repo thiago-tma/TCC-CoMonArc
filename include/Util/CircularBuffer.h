fb5fb9ef926d3238a17919e33a95fced404995b6
thiago-tma <thiagomotta@poli.ufrj.br>
2025-10-06 17:22:45 -0300

    CircularBuffer: Preparar malha de testes para desenvolver CircularBuffer genérico, Documentar interface do CircularBuffer

--- include/Util/CircularBuffer.h
+++ include/Util/CircularBuffer.h
@@ -5,12 +5,32 @@
 
 typedef struct CircularBufferStruct * CircularBuffer;
 
-CircularBuffer  CircularBuffer_Create              (unsigned int capacity);
-void            CircularBuffer_Destroy             (CircularBuffer buffer);
+/* @param buffer            Ponteiro para o CircularBuffer a ser iniciado */
+/* @param bufferStorage     Vetor de memória usado como buffer. O usuário deve garantir que o tamanho do buffer passado está correto*/
+/* @param elementsNumber    Capacidade de elementos do buffer                                             */
+/* @param elementSize       Número de bytes em um só elemento*/
+/* Retorna 0 com sucesso, -1 se um dos parâmetros for nulo                                                          */
+int  CircularBuffer_Create   (CircularBuffer buffer ,void * const bufferStorage, size_t elementsNumber, size_t elementSize);
+
+/* Restante das funções abaixo supõem um parâmetro CircularBuffer buffer válido  */
+
+/* Reinicia (Resets) o uso do buffer, tornando-o vazio  */
+/* Não tenta desalocar/liberar a memória do buffer      */
+/* Retorna 'true' com o sucesso da operação             */
+bool             CircularBuffer_Destroy             (CircularBuffer buffer);
+
+/* Retorna 'true' se vazio */
 bool            CircularBuffer_IsEmpty             (CircularBuffer buffer);
+
+/* Retorna 'true' se cheio */
 bool            CircularBuffer_IsFull              (CircularBuffer buffer);
-bool            CircularBuffer_Write               (CircularBuffer buffer, char     byte);
-bool            CircularBuffer_Read                (CircularBuffer buffer, char *   byte);
-bool            CircularBuffer_CheckIntegrity      (CircularBuffer buffer);
+
+/* Retorna 'true' se valor foi inserido */
+/* Retorna 'false* se buffer está cheio, descartando o valor*/
+bool            CircularBuffer_Write               (CircularBuffer buffer, void const  * elementIn);
+
+/* Retorna 'true' se valor foi lido     */
+/* Retorna 'false* se buffer está vazio */
+bool            CircularBuffer_Read                (CircularBuffer buffer, void *  elementOut);
 
 #endif  /* D_CircularBuffer_H */
--- tests/TestRunners/CircularBufferTest_Runner.c
+++ tests/TestRunners/CircularBufferTest_Runner.c
@@ -4,17 +4,4 @@
 TEST_GROUP_RUNNER(CircularBuffer)
 {
   RUN_TEST_CASE(CircularBuffer, CreateAndDestroy);
-  RUN_TEST_CASE(CircularBuffer, BufferReturnsFalseWhenReadingWhileEmpty);
-  RUN_TEST_CASE(CircularBuffer, ReturnNullIfCapacityIsZero);
-  RUN_TEST_CASE(CircularBuffer, BufferIsEmptyWhenCreated);
-  RUN_TEST_CASE(CircularBuffer, WriteAndRead);
-  RUN_TEST_CASE(CircularBuffer, WriteAndReadTwice);
-  RUN_TEST_CASE(CircularBuffer, BuffertNotEmptyAfterWrite);
-  RUN_TEST_CASE(CircularBuffer, BuffertEmptyAfterWriteAndRead);
-  RUN_TEST_CASE(CircularBuffer, IsBufferFull);
-  RUN_TEST_CASE(CircularBuffer, BufferIsEmptyAfterFull);
-  RUN_TEST_CASE(CircularBuffer, IntegrityAfterCreate);
-  RUN_TEST_CASE(CircularBuffer, IntegrityWhenFull);
-  RUN_TEST_CASE(CircularBuffer, IntegrityAfterWrapAround);
-  RUN_TEST_CASE(CircularBuffer, WrapAround);
 }
