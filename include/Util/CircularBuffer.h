8987485260adbc3aca5a53f10b3033b5a9552eb0
thiago-tma <thiagomotta@poli.ufrj.br>
2025-10-06 17:41:05 -0300

    CircularBuffer: Definir testes a serem realizados

--- include/Util/CircularBuffer.h
+++ include/Util/CircularBuffer.h
@@ -9,8 +9,8 @@ typedef struct CircularBufferStruct * CircularBuffer;
 /* @param bufferStorage     Vetor de memória usado como buffer. O usuário deve garantir que o tamanho do buffer passado está correto*/
 /* @param elementsNumber    Capacidade de elementos do buffer                                             */
 /* @param elementSize       Número de bytes em um só elemento*/
-/* Retorna 0 com sucesso, -1 se um dos parâmetros for nulo                                                          */
-int  CircularBuffer_Create   (CircularBuffer buffer ,void * const bufferStorage, size_t elementsNumber, size_t elementSize);
+/* Retorna true com sucesso, false se um dos parâmetros for nulo                                                          */
+bool  CircularBuffer_Create   (CircularBuffer buffer ,void * const bufferStorage, size_t elementsNumber, size_t elementSize);
 
 /* Restante das funções abaixo supõem um parâmetro CircularBuffer buffer válido  */
 
--- tests/Util/CircularBufferTest.c
+++ tests/Util/CircularBufferTest.c
@@ -5,18 +5,22 @@
  * Descrição: Buffer circular flexível, com definição do tamanho do elemento que entra e sai,
  *      mas não suporta elementos de diferentes tamanhos.
  * 
- * Inicialização retorna -1 se um dos parâmetros for nulo
- * Inicialiação retorna 1 se houver sucesso
+ * Inicialização retorna false se um dos parâmetros for nulo
+ * Inicialiação retorna true se houver sucesso
  * Buffer aponta estar vazio ao iniciar                     
  * Buffer retorna falso ao tentar ler com buffer vazio      
- * Buffer guarda e retorna o mesmo valor                   
+ * Buffer guarda e retorna o mesmo byte
+ * Buffer guarda e retorna um int32                   
  * Buffer aponta não-vazio após inserir valor               
  * Buffer aponta que está vazio após inserir e retirar valor
- * Buffer guarda e retorna um valor duas vezes              
- * Buffer apontar que está cheio corretamente               
+ * Buffer aponta que está vazio após inserir e destruir buffer
+ * Buffer guarda e retorna um byte duas vezes
+ * Buffer guarda e retorna um int32 duas vezes               
+ * Buffer aponta que está cheio corretamente               
  * Buffer aponta vazio após encher e esvaziar completamente 
- * Testar funcionalidades e estado (vazio/com elementos/cheio) após de inserir e retirar
- *      o equivalente à capacidade (wrap-around do buffer circular)   
+ * Buffer realiza o wrap-around corretamente com bytes
+ * Buffer realizao o wrap-around correntamente com int32
+
 */
 
 #include "unity.h"
@@ -33,7 +37,7 @@ TEST_SETUP(CircularBuffer)
 
 TEST_TEAR_DOWN(CircularBuffer)
 {
-    
+
 }
 
 TEST(CircularBuffer, CreateAndDestroy)
