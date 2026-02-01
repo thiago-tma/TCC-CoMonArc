2eda4cdc18b17e61c663d348c96d9c06032aeca2
thiago-tma <thiagomotta@poli.ufrj.br>
2026-02-01 16:49:02 -0300

    Magnetometer: Adicionar codigos de erro

--- lib/Magnetometer/Magnetometer.h
+++ lib/Magnetometer/Magnetometer.h
@@ -7,15 +7,24 @@ extern "C" {
 
 #include <stdint.h>
 
-void Magnetometer_Create();
-void Magnetometer_Destroy();
+typedef enum
+{
+    MAGNETOMETER_OK,
+    MAGNETOMETER_ERROR_NOT_INITIALIZED,
+    MAGNETOMETER_ERROR_ALREADY_INITIALIZED,
+    MAGNETOMETER_ERROR_INITIALIZATION_FAILED,
+    MAGNETOMETER_ERROR_NEWREAD_FAILED
+} Magnetometer_Error_t;
+
+Magnetometer_Error_t Magnetometer_Create(void);
+Magnetometer_Error_t Magnetometer_Destroy(void);
 
 /* Leitura assíncrona do sensor magnetômetro */
 /* Traduz leitura para os formatos utilizados pelo sistema */
-void Magnetometer_NewRead();
+Magnetometer_Error_t Magnetometer_NewRead(void);
 
 /* heading é uma variável de valor entre 0-359, indicando a direção no plano horizontal */
-void Magnetometer_GetHeading(int16_t * heading);
+Magnetometer_Error_t Magnetometer_GetHeading(int16_t * heading);
 
 #ifdef __cplusplus
 }
--- src/ArduinoCore/Common/Magnetometer/QMC5883P.cpp
+++ src/ArduinoCore/Common/Magnetometer/QMC5883P.cpp
@@ -3,7 +3,11 @@
 #include <Adafruit_QMC5883P.h>
 #include <Logger/include/log_api.h>
 
-Adafruit_QMC5883P qmc;
+#define MAGNETOMETER_INITIALIZATION_MAX_ATTEMPTS 10
+
+static bool initialized = false;
+
+static Adafruit_QMC5883P qmc;
 
 /* Values acquired from calibration for QMC5883 1 */
 //static int16_t Xmin = -9990;
@@ -12,23 +16,26 @@ Adafruit_QMC5883P qmc;
 //static int16_t Ymax = -180;
 
 /* Values acquired from calibration for QMC5883 2 */
-static int16_t Xmin = -3413;
-static int16_t Xmax = 6592;
-static int16_t Ymin = -8963; 
-static int16_t Ymax = 1252;
+static int16_t Xmin = -2670;
+static int16_t Xmax = 2025;
+static int16_t Ymin = -7125; 
+static int16_t Ymax = -2363;
 
 static int16_t Xoffset = 0;
 static int16_t Yoffset = 0;
 
 static float savedHeading = 0;
 
-void Magnetometer_Create()
+Magnetometer_Error_t Magnetometer_Create()
 {
+    if (initialized) return MAGNETOMETER_ERROR_ALREADY_INITIALIZED;
 
-    if (!qmc.begin())
+    uint8_t tries = 0;
+    while (!qmc.begin() && tries < MAGNETOMETER_INITIALIZATION_MAX_ATTEMPTS)
     {
-        while(1);
+        tries++;
     }
+    if (tries >= MAGNETOMETER_INITIALIZATION_MAX_ATTEMPTS) return MAGNETOMETER_ERROR_INITIALIZATION_FAILED;
     
     /* Calibration offset */
     Xoffset = (Xmax + Xmin) / 2;
@@ -40,20 +47,30 @@ void Magnetometer_Create()
     qmc.setDSR(QMC5883P_DSR_2);
     qmc.setODR(QMC5883P_ODR_50HZ);
     qmc.setMode(QMC5883P_MODE_CONTINUOUS);
+
+    initialized = true;
+
+    return MAGNETOMETER_OK;
 }
 
-void Magnetometer_Destroy()
+Magnetometer_Error_t Magnetometer_Destroy()
 {
+    if (!initialized) return MAGNETOMETER_ERROR_NOT_INITIALIZED;
+
     qmc.softReset();
     savedHeading = 0;
+
+    return MAGNETOMETER_OK;
 }
 
-void Magnetometer_NewRead()
+Magnetometer_Error_t Magnetometer_NewRead()
 {
+    if (!initialized) return MAGNETOMETER_ERROR_NOT_INITIALIZED;
+
     int16_t x, y, z;
 
     /* Read raw magnetic field data */
-    qmc.getRawMagnetic(&x, &y, &z);
+    if (!qmc.getRawMagnetic(&x, &y, &z)) return MAGNETOMETER_ERROR_NEWREAD_FAILED;
 
     /* Apply hard-iron calibration correction */
     x -= Xoffset;
@@ -65,9 +82,14 @@ void Magnetometer_NewRead()
 
     savedHeading = (int16_t)heading;
     log_magnetometer_data_reading(heading);
+
+    return MAGNETOMETER_OK;
 }
 
-void Magnetometer_GetHeading(int16_t * heading)
+Magnetometer_Error_t Magnetometer_GetHeading(int16_t * heading)
 {
+    if (!initialized) return MAGNETOMETER_ERROR_NOT_INITIALIZED;
+
     *heading = savedHeading;
+    return MAGNETOMETER_OK;
 }
